1be1f48adcf9b466f0164677160bf3a55ebfbfaa
inacioads23 <jinacio11@gmail.com>
2025-02-18 17:01:03 -0300

    Update project

---
+++ src/main/java/com/devsoft/rgdi_store/RgdiStoreApplication.java
@@ -0,0 +1,13 @@
+package com.devsoft.rgdi_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RgdiStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RgdiStoreApplication.class, args);
+	}
+
+}
