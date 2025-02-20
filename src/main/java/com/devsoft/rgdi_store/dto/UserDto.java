53c72c17f93cff4c0b7a316790cc6f4e91caf470
inacioads23 <jinacio11@gmail.com>
2025-02-20 20:28:28 -0300

    Created UserRepository

--- src/main/java/com/devsoft/rgdi_store/dto/UserDto.java
+++ src/main/java/com/devsoft/rgdi_store/dto/UserDto.java
@@ -95,6 +95,5 @@ public class UserDto {
 	public void setGrupo(UserGroup grupo) {
 		this.grupo = grupo;
 	}
-
 	
 }
---
+++ src/main/java/com/devsoft/rgdi_store/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.devsoft.rgdi_store.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsoft.rgdi_store.entities.UserEntity;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long>{
+
+}
