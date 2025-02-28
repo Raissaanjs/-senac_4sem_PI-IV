737dd6e910b1746f71c73d8303b89aa3c816edf8
inacioads23 <jinacio11@gmail.com>
2025-02-28 12:43:15 -0300

    Cleaning project

--- src/main/java/com/devsoft/rgdi_store/repositories/UserRepository.java
+++ src/main/java/com/devsoft/rgdi_store/repositories/UserRepository.java
@@ -1,12 +1,9 @@
 package com.devsoft.rgdi_store.repositories;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsoft.rgdi_store.entities.UserEntity;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long>{
-	//Optional<UserEntity> findByEmail(String email);
 
 }
