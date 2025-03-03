43335d7b4b9a8a903f6886d183aba866697c597d
inacioads23 <jinacio11@gmail.com>
2025-03-03 08:20:21 -0300

    Update lista

---
+++ src/main/java/com/devsoft/rgdi_store/controllers/MainController.java
@@ -0,0 +1,24 @@
+package com.devsoft.rgdi_store.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String login() {
+        return "login"; // Redireciona para o arquivo login.html
+    }
+    @GetMapping("/auth")
+    public String home() {
+        return "home"; // Redireciona para o arquivo home.html
+    }
+
+    @GetMapping("/h2-console")
+    public String redirectToH2Console() {
+        return "redirect:/h2-console"; // Redireciona para o console do H2
+    }
+
+}
+
