19d8f5c81d3c09309c611e8becd4e261b366a91a
inacioads23 <jinacio11@gmail.com>
2025-03-10 17:46:37 -0300

    Seeding db

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,1 +1,2 @@
 INSERT INTO tb_usuario (nome, cpf, email, senha, status, grupo) VALUES ('Sabrina Rufino de Oliveira', '11002233449', 'sabrina@test.com', '$2a$12$IbljZ5w4k5zjtrmGISaOreExa4oU.J6y7UxGKBFnBSuzsdt.V9Nv.', 'TRUE', 'ROLE_ADMIN');
+INSERT INTO tb_usuario (nome, cpf, email, senha, status, grupo) VALUES ('Samira Rufino', '22002233449', 'samira@test.com', '$2a$12$KBCWxK00SuaZL/baVXOtcuKVLic/5pjwYbKWUclevyRWQfeWtvnGK', 'TRUE', 'ROLE_ESTOQ');
