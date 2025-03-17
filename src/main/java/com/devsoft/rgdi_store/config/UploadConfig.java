f6aa759d61564a19a1af47f2c662e7a776aba1aa
inacioads23 <jinacio11@gmail.com>
2025-03-17 17:44:54 -0300

    Adjusts Product

--- src/main/java/com/devsoft/rgdi_store/config/UploadConfig.java
+++ src/main/java/com/devsoft/rgdi_store/config/UploadConfig.java
@@ -1,15 +1,19 @@
 package com.devsoft.rgdi_store.config;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ConfigurationProperties(prefix = "file")
 public class UploadConfig {
+    
+	private String uploadDir;
 
-    @Value("${upload.dir}")
-    private String uploadDir;
+	  public String getUploadDir() {
+	    return uploadDir;
+	  }
 
-    public String getUploadDir() {
-        return uploadDir;
-    }
+	  public void setUploadDir(String uploadDir) {
+	    this.uploadDir = uploadDir;
+	  }
 }
---
+++ src/main/java/com/devsoft/rgdi_store/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.devsoft.rgdi_store.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private final UploadConfig uploadConfig; // A classe UploadConfig fornece o diretório de upload
+
+    public WebConfig(UploadConfig uploadConfig) {
+        this.uploadConfig = uploadConfig;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // Usando o uploadConfig para pegar o diretório de uploads
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:" + uploadConfig.getUploadDir() + "/");
+    }
+}
