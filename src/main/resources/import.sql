d43906c045d843287f22d37cc59f525e532ac032
inacioads23 <jinacio11@gmail.com>
2025-03-19 17:52:15 -0300

    Seeding DB

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,4 +2,6 @@ INSERT INTO tb_usuario (nome, cpf, email, senha, status, grupo) VALUES ('Sabrina
 INSERT INTO tb_usuario (nome, cpf, email, senha, status, grupo) VALUES ('Samira de Oliveira', '22002233449', 'samira@test.com', '$2a$12$KBCWxK00SuaZL/baVXOtcuKVLic/5pjwYbKWUclevyRWQfeWtvnGK', 'TRUE', 'ROLE_ESTOQ');
 INSERT INTO tb_usuario (nome, cpf, email, senha, status, grupo) VALUES ('Matheus de Jesus', '11002233401', 'matheus@test.com', '$2a$12$CNVfjO6AHGJpgdh5PyB0pO8gVNBzDw8KlTWzBWkW9fdDkWN6qRpFi', 'TRUE', 'ROLE_USER');
 
-
+INSERT INTO tb_produto (nome, preco, quantidade, descricao, avaliacao, status) VALUES ('Cimento 50Kg', 50.0, 10, 'Cimento 50Kg - Votoran', 5, 'TRUE');
+INSERT INTO tb_produto (nome, preco, quantidade, descricao, avaliacao, status) VALUES ('Porta de Alumínio - 2,10X0,70', 200.0, 10, 'Porta de Alumínio - 2,10X0,70 - Metalfort', 4, 'TRUE');
+INSERT INTO tb_produto (nome, preco, quantidade, descricao, avaliacao, status) VALUES ('Areia - Mt', 120.0, 100, 'Areia Média por Metro', 5, 'TRUE');
