dddcf1134bfa1c6be270c68d6222ddca28c907e7
Guilherme Wilson <126832745+Gwiill@users.noreply.github.com>
2025-05-08 12:06:54 -0300

    Register Endereco

---
+++ src/main/resources/static/js/cadastroEnderecoInicial.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded', function () {
+	    const btnAdicionarEntrega = document.getElementById('btnAdicionarEntrega');
+	    const enderecoEntregaSection = document.getElementById('enderecoEntregaSection');
+	    const formFaturamento = document.getElementById('formFaturamento');
+	    const clienteId = /*[[${clienteId}]]*/ null; // Recupera o clienteId do Thymeleaf
+	
+	    if (formFaturamento) {
+	        formFaturamento.addEventListener('submit', function (event) {
+	            event.preventDefault();
+	
+	            const formData = new FormData(formFaturamento);
+	            fetch('/enderecos/noauth/endereco-inicial/faturamento', {
+	                method: 'POST',
+	                body: formData,
+	            })
+	                .then(response => response.json())
+	                .then(data => {
+	                    enderecoEntregaSection.style.display = 'block';
+	                    btnAdicionarEntrega.disabled = true;
+	                })
+	                .catch(error => {
+	                    console.error('Erro ao salvar endereço de faturamento:', error);
+	                    alert('Erro ao salvar endereço de faturamento.');
+	                });
+	        });
+	    } else {
+	        console.warn('formFaturamento não encontrado no DOM.');
+	    }
+	});
+	
+	//Preencher o Endereço de Entrega como o mesmo do faturamento
+	function copiarEnderecoFaturamento() {
+	    const usarMesmoEndereco = document.getElementById("copiarEndereco").checked;
+	    const campos = ["cep", "logradouro", "numero", "complemento", "bairro", "cidade", "uf"];
+	    campos.forEach(campo => {
+	        const origem = document.getElementById(`${campo}Faturamento`);
+	        const destino = document.getElementById(`${campo}Entrega`);
+	        if (origem && destino) {
+	            destino.value = usarMesmoEndereco ? origem.value : '';
+	        }
+	    });
+	}
