b0b37219d80d7fd0af8642aaf15a892350ee2150
inacioads23 <jinacio11@gmail.com>
2025-05-24 20:44:40 -0300

    Update General

--- src/main/resources/import.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO tb_usuario (nome, cpf, email, senha, status, grupo) VALUES ('Sabrina Rufino de Oliveira', '39025898092', 'sabrina@test.com', '$2a$12$IbljZ5w4k5zjtrmGISaOreExa4oU.J6y7UxGKBFnBSuzsdt.V9Nv.', true, 'ROLE_ADMIN') 
